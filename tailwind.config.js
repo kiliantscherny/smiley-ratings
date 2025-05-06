d45a3b7b55edcbfec1dfb0a426d275283b313abc
Kilian Tscherny <90620239+kiliantscherny@users.noreply.github.com>
2025-05-06 11:45:10 +0200

    fix build errors

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,svelte,ts}',
+      './node_modules/@evidence-dev/**/*.{html,js,svelte,ts}',
+      './.evidence/template/**/*.{html,js,svelte,ts}'
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
