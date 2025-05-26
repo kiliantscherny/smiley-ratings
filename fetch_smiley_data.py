fd73dfcb6906c5c792cf54bf4b7927ece661c369
Kilian Tscherny <90620239+kiliantscherny@users.noreply.github.com>
2025-05-26 10:23:33 +0200

    update fetch smiley data script with better logging

--- fetch_smiley_data.py
+++ fetch_smiley_data.py
@@ -1,34 +1,14 @@
-import logging
 from datetime import datetime
 
 import duckdb
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-from colorlog import ColoredFormatter
-
-
-def setup_logging():
-    formatter = ColoredFormatter(
-        "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "bold_red",
-        },
-    )
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logging.basicConfig(
-        level=logging.INFO,
-        handlers=[handler, logging.FileHandler("download.log")],
-    )
+from loguru import logger
 
 
 def download_smiley_excel() -> str:
+    logger.debug("Checking website for Excel file link...")
     page_url = "https://www.findsmiley.dk/Statistik/Smiley_data/Sider/default.aspx"
     response = requests.get(page_url)
 
@@ -42,42 +22,44 @@ def download_smiley_excel() -> str:
 
         if link and link["href"]:
             excel_url = link["href"]
-            logging.info(f"Found Excel file URL: {excel_url}")
+            logger.success(f"Found Excel file URL: {excel_url}")
             excel_response = requests.get(excel_url)
             if excel_response.status_code == 200:
                 file_name = f"smiley_{datetime.now().strftime('%Y%m%d')}.xlsx"
                 with open(file_name, "wb") as file:
                     file.write(excel_response.content)
-                logging.info(f"Excel file downloaded as '{file_name}'")
+                logger.info(f"Excel file downloaded as '{file_name}'")
                 return file_name
             else:
-                logging.error(
+                logger.error(
                     f"Failed to download Excel file: {excel_response.status_code}"
                 )
         else:
-            logging.error("Excel link not found.")
+            logger.critical("Excel link not found.")
     else:
-        logging.error(f"Failed to fetch page: {response.status_code}")
+        logger.critical(f"Failed to fetch page: {response.status_code}")
     return None
 
 
 def excel_to_duckdb(
     excel_file: str, db_path: str = "sources/smiley_data/smiley_data.duckdb"
-):
+) -> None:
+    logger.debug("Converting Excel to Pandas dataframe...")
     df = pd.read_excel(excel_file)
+    logger.debug("Creating DuckDB table from dataframe...")
     con = duckdb.connect(database=db_path)
     con.register("smiley_df", df)
     try:
         con.execute("CREATE OR REPLACE TABLE smiley_ratings AS SELECT * FROM smiley_df")
-        logging.info("Table created successfully.")
+        logger.success("Table created successfully.")
+        logger.info("Tables in database:")
+        logger.info(con.execute("SHOW TABLES").fetchdf())
     except Exception as e:
-        logging.error(f"Error creating table: {e}")
-    logging.info(con.execute("SHOW TABLES").fetchdf())
+        logger.critical(f"Error creating table: {e}")
     con.close()
 
 
 if __name__ == "__main__":
-    setup_logging()
     file_name = download_smiley_excel()
     if file_name:
         excel_to_duckdb(file_name)
